27e4cc358d3d649fb44bbbeaf786873085a36ca1
Gustavo <gufavmartins@hotmail.com>
2025-07-21 23:06:11 -0300

    style: reordered imports

--- app/src/main/java/com/mekki/taco/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/mekki/taco/presentation/navigation/AppNavigation.kt
@@ -11,6 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
