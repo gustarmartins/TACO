c396082ab3148fd23cfa9f4a1e57d40bcc7754b7
Gustavo <gufavmartins@hotmail.com>
2025-07-21 23:06:31 -0300

    style: made imports with less noise (cleaner)

--- app/src/main/java/com/mekki/taco/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/mekki/taco/presentation/navigation/AppNavigation.kt
@@ -1,10 +1,7 @@
 package com.mekki.taco.ui
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
