762720cf5e7e9673b9914c3563817fd2d357e2a1
Gustavo Martins <gufavmartins@hotmail.com>
2025-09-04 16:57:04 -0300

    Attempt at fixing invisible Android UI icons

--- app/src/main/java/com/mekki/taco/presentation/ui/components/CommonUI.kt
+++ app/src/main/java/com/mekki/taco/presentation/ui/components/CommonUI.kt
@@ -3,6 +3,7 @@ package com.mekki.taco.presentation.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,8 +12,17 @@ import java.text.DecimalFormat
 @Composable
 fun NutrientRow(label: String, value: Double, unit: String) {
     val df = DecimalFormat("#.#")
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-        Text(label)
-        Text("${df.format(value)} $unit")
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = label,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Text(
+            text = "${df.format(value)} $unit",
+            color = MaterialTheme.colorScheme.onBackground
+        )
     }
 }
